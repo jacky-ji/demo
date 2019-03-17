98ae48a3d03ad66b816a7a7eed913948b639394c
Jacky.Ji <13918685816@163.com>
2019-03-17 11:07:58 +0800

    Added Test Class

---
+++ demo/src/main/java/demo/Test.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * Created by admin on 2019/3/17.
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Only for testing");
+    }
+}
